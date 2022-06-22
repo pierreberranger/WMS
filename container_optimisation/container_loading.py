433091e739d28170b285280c706971990f94bc8f
Virgile-Richard1 <virgile.richard@minesparis.psl.eu>
2022-06-22 14:12:36 +0200

    Repo container_optimization

---
+++ container_optimisation/container_loading.py
@@ -0,0 +1,121 @@
+from typing import Tuple
+from models import Groupage, Container, Package, Shipment, Dimensions, Trip
+import pickle_data as database
+from matplotlib import pyplot as plt
+
+from rectpack import newPacker, SkylineMwfl, GuillotineBssfSas, MaxRectsBaf, SkylineBlWm
+import random
+
+def package_area(package: Package) -> int:
+    """Computes the area on the ground of a package in cm^2"""
+    return package.dimensions.width * package.dimensions.length
+
+def container_loading(groupage: Groupage, available_containers_id: set[str]) -> Tuple[set[str], list]:
+    """Given a groupage and a set of available_container_ids, loads containers with the given groupage.
+    Returns:
+    - a set of the used container ids
+    - the placement of each package within those containers"""
+
+
+    # Container dimensions in cm (rounded low)
+    width, length, height = 235, 589, 239 # Standart (not fit for EU palet)
+    container_area: int = width*length
+
+    for shipment in groupage.set_of_shipments:
+        shipment_minimal_area = sum([package_area(package) for package in shipment.set_of_packages])
+        if shipment_minimal_area >= 0.76*container_area:
+            # we are sure that the shipment will fit in more than one container
+            pass
+
+    rectangles = [(package.dimensions.width, package.dimensions.length, package.id) for shipment in groupage.set_of_shipments
+                                                for package in shipment.set_of_packages]
+    
+
+    random.shuffle(rectangles)
+
+    bins = [database.set_of_containers[container_id].dimensions[:2] for container_id in available_containers_id]
+
+    packer = newPacker(pack_algo=SkylineBlWm, sort_algo=lambda rectlist: sorted(rectlist, reverse=True, 
+        key=lambda width_length_id: (database.set_of_packages[width_length_id[2]].shipment_id, 
+                                        width_length_id[0]*width_length_id[1])))
+
+    # Add the rectangles to packing queue
+    for width, length,id in rectangles:
+        packer.add_rect(width,length,id)
+
+    # Add the bins where the rectangles will be placed
+    for b in bins:
+        packer.add_bin(*b)
+
+    # Start packing
+    packer.pack()
+
+    package_placements = packer.rect_list()
+
+    for i, container_id in enumerate(available_containers_id):
+        for j, package_placement in enumerate(package_placements):
+            if package_placement[0] == i:
+                _, *args = package_placement
+                package_placements[j] = tuple([container_id, *args])
+
+    return {package_placement[0] for package_placement in package_placements}, package_placements
+
+def trip_loading(trip_id: str, available_containers_id: set[str]) -> dict:
+    trip: Trip = database.set_of_trips[trip_id]
+    groupage_placements = dict()
+    for groupage in trip.set_of_groupages:
+        groupage_placements[groupage.id] = container_loading(groupage, available_containers_id)
+        available_containers_id = available_containers_id.difference(groupage_placements[groupage.id][0])
+    for groupage in trip.set_of_groupages:
+        plot_container_load_output(*groupage_placements[groupage.id], groupage.id)
+    return groupage_placements
+
+def plot_container_load_output(container_ids: set[str], package_placements: list, groupage_id: str = None) -> None:
+
+    nb_containers = len(container_ids)
+
+    fig = plt.figure(figsize=(5*nb_containers, 7))
+
+    plt.rc('font', **{'size': 5})
+
+    for container_idx, container_id in enumerate(container_ids): 
+        ax = fig.add_subplot(1, nb_containers, (container_idx+1))
+        container_dimensions = database.set_of_containers[container_id].dimensions[:2]
+
+        # Draw the container limits 
+        plt.plot([0, container_dimensions[0], container_dimensions[0], 0, 0], 
+                [0, 0, container_dimensions[1], container_dimensions[1], 0], '--r')
+
+        for package_placement in package_placements:
+            if package_placement[0] == container_id:
+                container_id, x, y, w, l, package_id = package_placement
+                x1, x2, x3, x4, x5 = x, x+w, x+w, x, x
+                y1, y2, y3, y4, y5 = y, y, y+l, y+l,y
+
+                plt.plot([x1,x2,x3,x4,x5],[y1,y2,y3,y4,y5], '--k')
+
+                package = database.set_of_packages[package_id]
+                plt.annotate(f"{package_id} ({package.shipment_id})", (x+w/3, y+l/2), color='b')
+
+        ax.set_aspect('equal')
+        ax.set_title(f"{container_id}", size=15, weight='bold')
+        plt.axis('off')
+
+    # espacement entre les subplots
+    fig.tight_layout(pad=10.0)
+    if not(groupage_id is None):
+        plt.suptitle(f"{groupage_id}", size=20)
+
+    plt.show()
+
+def validate_container_loading_proposal(package_placements: list) -> None:
+    for container_id, _, _, width, length, package_id in package_placements:
+        package: Package = database.set_of_packages[package_id]
+        package.dimensions = Dimensions(width, length, package.dimensions.height)
+        package.container_id = container_id
+
+def validate_trip_loading_proposal(groupage_placements: dict) -> None:
+    for groupage_id, (containers_id, package_placements) in groupage_placements.items():
+        for container_id in containers_id:
+            database.set_of_containers[container_id].groupage_id = groupage_id
+        validate_container_loading_proposal(package_placements)
---
+++ container_optimisation/test_containter_opti.py
@@ -0,0 +1,43 @@
+import unittest
+
+import pickle_data as database
+from models import Dimensions, Package
+from container_opti import define_positions, package_extrema 
+
+
+class TestOptimisation(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.package1 = Package(Dimensions(1, 2, 1), None, None, None)
+        self.package2 = Package(Dimensions(1, 0.7, 1), None, None, None)
+        self.package3 = Package(Dimensions(1, 1.1, 1), None, None, None)
+        self.package4 = Package(Dimensions(1, 0.9, 1), None, None, None)
+        self.package5 = Package(Dimensions(1, 0.9, 1), None, None, None)
+        self.package6 = Package(Dimensions(1, 1.5, 1), None, None, None)
+        self.package7 = Package(Dimensions(1, 1.5, 1), None, None, None)
+
+    def test_optimisation2D_pos(self):
+        args = {0: [self.package1.id], 1: [self.package2.id, self.package3.id], 2: [self.package4.id, self.package5.id, self.package6.id], 3: [self.package7.id]}
+        res = define_positions(args)
+
+        for id_to_check, priority_to_check, x, y, is_turned in res:
+            package_to_check: Package = database.set_of_packages[other_id]
+            x_length, y_length = package_extrema(package_to_check, is_turned)
+            for other_id, other_priority, other_x, other_y, other_is_turned in res:
+                if other_priority < priority_to_check:
+                    other_package: Package = database.set_of_packages[other_id]
+                    other_x_length, other_y_length = package_extrema(other_package, other_is_turned)
+
+    def test_opti_1D(self):
+        args = {0: [self.package1.id], 1: [self.package3.id, self.package2.id], 2: [self.package6.id, self.package4.id, self.package5.id], 3: [self.package7.id]}
+        res = define_positions(args)
+
+        self.assertAlmostEqual(res, [(self.package1.id, 0, 0, 0, False),
+                                    (self.package2.id, 0, 2, 0, False), 
+                                    (self.package3.id, 1, 0, 0, False), 
+                                    (self.package4.id, 1, 1.1, 0, False), 
+                                    (self.package5.id, 1, 2, 0, False), 
+                                    (self.package6.id, 2, 0, 0, False), 
+                                    (self.package7.id, 2, 1.5, 0, False), 
+            ]
+        )
