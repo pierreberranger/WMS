110b0462ff34aa89b22b3b77d526dd2572639f1b
pierreberranger <pierre@berranger.eu>
2022-06-29 16:53:29 +0200

    problème de merge: suppression d'une redondance dans plan loading entrainant un bug

--- interface_commands.py
+++ interface_commands.py
@@ -452,7 +452,6 @@ def plan_loading() -> None:
     trip_id = prompt.trip_id()
     available_containers = select_available_containers()
     groupage_placements = container_loading.trip_loading(trip_id, available_containers)
-    display.show_fig(groupage_placements)
     click.echo(f"We propose you the plannification for the load of the trip n°{trip_id} '\n' look at the pdf associated to your trip in the outuput file before validate")
     if confirm.plan_loading() :
         display.save_trip_loading_proposal(groupage_placements, trip_id)
